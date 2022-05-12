40a564e14beeaa758b606cd1062ba0dda2ab558b
nnamss <10407499+nnamss@users.noreply.github.com>
2022-05-12 18:13:49 -0400

    build

--- dist/model/volumeReport.d.ts
+++ dist/model/volumeReport.d.ts
@@ -1,12 +1,17 @@
-interface CollectionReport {
+export interface CollectionReport {
     id: number;
     name: string;
     image: string;
     volume: number;
 }
+export interface ReleaseCollection {
+    name: string;
+    image: string;
+}
 interface ReleaseReport {
     id: number;
     volume: number;
+    collection: ReleaseCollection | null;
 }
 export default interface VolumeReport {
     totalSales: number;
